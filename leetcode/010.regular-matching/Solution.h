59017a254ec5d9f0eb2c122e42182f2e95ddf0dc
niumeng07 <myliuda@sina.com>
2017-03-26 22:32:34 +0800

    nothing

--- leetcode/010.regular-matching/Solution.h
+++ leetcode/010.regular-matching/Solution.h
@@ -5,14 +5,7 @@
 using namespace std;
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int i = 0, j = 0, s_len = s.length(), p_len = p.length();            
-        if( p_len == 0 && s_len == 0 ) return true;
-        if( p_len == 0 && s_len != 0 ) return false;
-        if( char(p[0]) == '*' ) return false;
-        bool flag = true;
-        for( ; i < s_len && j < p_len; ){
-
-        }
+    bool isMatch(string s, string p) 
+    {
     }
 };
