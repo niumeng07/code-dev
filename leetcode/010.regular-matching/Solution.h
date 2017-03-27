07114c01f06cef098d42af1ea6f6fa8b550b1acf
niumeng07 <myliuda@sina.com>
2017-03-27 23:57:42 +0800

    regular-matching

--- leetcode/010.regular-matching/Solution.h
+++ leetcode/010.regular-matching/Solution.h
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <map>
 #include <string>
-#include <vector>
 using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) 
     {
+        if( p.empty() ) 
+            return s.empty();
+        if( char(p[p.length() -1 ]) == '*' && ( char(s[s.length() - 1]) == char(p[p.length() -2 ]) || (char(p[p.length() -2 ]) == '.')))// p未位为*
+            return (!s.empty()) ? isMatch(s, p.substr(0, p.length() - 2)) || isMatch( s.substr(0, s.length() -1 ), p.substr(0, p.length() -2 )) || isMatch(s.substr(0, s.length() -1 ), p ) : isMatch(s, p.substr(0, p.length() - 2));
+        if( char(p[p.length() -1 ]) == '*' && char(s[s.length() - 1]) != char(p[p.length() -2 ])) //p未位为* 
+            return isMatch(s.substr(0, s.length() ), p.substr(0, p.length() -2 ));
+        if( char(p[p.length() -1 ]) == '.' )// p未位.
+            return (s.empty())? false : isMatch(s.substr(0, s.length() - 1), p.substr(0, p.length() - 1));
+        if(char(p[p.length() -1 ]) != '.' && char(p[p.length() -1 ]) != '*')//p未尾非.非*
+            return (p[p.length() - 1 ] == s[s.length() - 1]) ? isMatch(s.substr(0, s.length() -1 ), p.substr(0, p.length() - 1)) : false;
     }
 };
--- leetcode/010.regular-matching/main.cpp
+++ leetcode/010.regular-matching/main.cpp
@@ -5,10 +5,20 @@ using namespace std;
 int main()
 {
     Solution su;
-    su.isMatch("aa", "a");
-    su.isMatch("aa", "aa");
-    su.isMatch("aaa", "aa");
-    su.isMatch("aa", "a*");
-
+    cout << "re: " << su.isMatch("ab", ".*c")<< endl;
+    cout << "re: " << su.isMatch("", "..*")<< endl;
+    cout << "re: " << su.isMatch("a", ".*..a*")<< endl;
+    cout << "re: " << su.isMatch("aaa", "ab*a")<< endl;
+    cout << "re: " << su.isMatch("aaa", ".a")<< endl;
+    cout << "re: " << su.isMatch("aa", ".*")<< endl;
+    cout << "re: " << su.isMatch("aa", "a") << endl;
+    cout << "re: " << su.isMatch("aa", "aa")<< endl;
+    cout << "re: " << su.isMatch("aaa", "aa")<< endl;
+    cout << "re: " << su.isMatch("aa", "a*")<< endl;
+    cout << "re: " << su.isMatch("ab", ".*")<< endl;
+    cout << "re: " << su.isMatch("aab", "c*a*b*")<< endl;
     return 0;
 }
+// aaa, ab*a
+// aa, ab*
+//
