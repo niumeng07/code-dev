c9788b3daab319cbd1c5f36619fe64c169d0a158
niumeng07 <myliuda@sina.com>
2017-03-29 22:22:43 +0800

    009.PalindromeNumber

---
+++ leetcode/009.PalindromeNumber/Solution.h
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(int x) 
+    {
+        int x_ori =x;
+        if ( x < 0) return false;
+        long result = 0;
+        while(x!= 0)
+        {
+            result = result * 10 + x % 10;
+            x = x/10;
+        }
+        if ( (long)x_ori == result ) return true;
+        return false;
+    }
+};
---
+++ leetcode/009.PalindromeNumber/main.cpp
@@ -0,0 +1,10 @@
+#include "Solution.h"
+
+int main()
+{
+    Solution su;
+    cout << su.isPalindrome(12345) << endl;
+    cout << su.isPalindrome(12345783) << endl;
+    cout << su.isPalindrome(1) << endl;
+    return 0;
+}
