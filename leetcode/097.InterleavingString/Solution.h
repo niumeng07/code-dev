a80c0813d22bb81318617af5877ca204a27537e6
niumeng07 <myliuda@sina.com>
2017-04-16 22:40:20 +0800

    097

---
+++ leetcode/097.InterleavingString/Solution.h
@@ -0,0 +1,49 @@
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    /*
+     * 下面是循环解法，缺点是会不断循环，耗时极大
+     * */
+    bool isInterleave(string s1, string s2, string s3)
+    {
+        if( s1.size() + s2.size() != s3.size()) return false;
+        if( s1.size() == 0 ) return s2==s3;
+        if( s2.size() == 0 ) return s1==s3;
+        if( char(s1[0]) == char(s3[0]) && char(s2[0]) != char(s3[0]) ) 
+            return isInterleave(s1.substr(1, s1.size() - 1), s2, s3.substr(1, s3.size() - 1));
+        if( char(s2[0]) == char(s3[0]) && char(s1[0]) != char(s3[0])) 
+            return isInterleave(s1, s2.substr(1, s2.size() - 1), s3.substr(1, s3.size() - 1));
+        if( char(s2[0]) == char(s3[0]) && char(s1[0]) == char(s3[0]))
+            return isInterleave(s1.substr(1, s1.size() - 1), s2, s3.substr(1, s3.size() - 1)) || isInterleave(s1, s2.substr(1, s2.size() - 1), s3.substr(1, s3.size() - 1));
+        return false;
+    }
+    /*
+     * 动态规划算法，把前面的结果保存起来，后面接着用
+     * */
+	bool isInterleaveDP(string s1, string s2, string s3) {
+		
+		if(s3.length() != s1.length() + s2.length())
+			return false;
+        if(s1.empty()) return s2 == s3;
+        if(s2.empty()) return s1 == s3;
+		
+		bool table[s1.length()+1][s2.length()+1];
+		
+		for(int i=0; i<s1.length()+1; i++)
+			for(int j=0; j< s2.length()+1; j++){
+				if(i==0 && j==0)
+					table[i][j] = true;
+				else if(i == 0)
+					table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
+				else if(j == 0)
+					table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+				else
+					table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+			}
+			
+		return table[s1.length()][s2.length()];
+	}
+};
+
