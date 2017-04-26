9aa026641fee5afb86d6354883b942b0285b96df
niumeng07 <myliuda@sina.com>
2017-04-26 23:04:42 +0800

    141.LinkListCycle python

---
+++ leetcode/141.LinkListCycle/LinkListCycle.py
@@ -0,0 +1,39 @@
+import pdb
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        try:
+            v_set = set([])
+            iter = head
+            v_set.update([str(iter)])
+            while iter.next != None:
+                iter = iter.next
+                if str(iter) not in v_set:
+                    v_set.update([str(iter)])
+                else:
+                    return True
+            return False
+        except:
+            return False
+
+if __name__ == "__main__":
+    first = ListNode("first")
+    head = ListNode("head")
+    head.next = first
+
+    su = Solution()
+    if su.hasCycle(head):
+        print "has Cycle"
+    else:
+        print "no Cycle"
+
+    first.next = head
+    if su.hasCycle(head):
+        print "has Cycle"
+    else:
+        print "no Cycle"
+
