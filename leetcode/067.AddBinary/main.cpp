9e13c85c0fd16e9641ce3b82120b040d7c0908e2
niumeng07 <myliuda@sina.com>
2017-06-12 23:38:23 +0800

    067.AddBinary end

---
+++ leetcode/067.AddBinary/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Solution.h"
+
+using namespace std;
+
+int main()
+{
+    string a = "1234";
+    Solution su;
+    cout << su.addBinary("1", "111") << endl;
+    return 0;
+}
+// 1,3
+// 0 2
+// -1 1
