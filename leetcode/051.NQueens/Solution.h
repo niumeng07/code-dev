7394679ac3a84b72c8c50ff2481daf601ab1d7be
niumeng07 <myliuda@sina.com>
2017-06-13 08:19:24 +0800

    051.NQueens init

--- leetcode/051.NQueens/Solution.h
+++ leetcode/051.NQueens/Solution.h
@@ -1,6 +1,10 @@
+#include <vector>
+using namespace std;
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        
+        vector<vector<string> > res;
+
+        return res;
     }
 };
---
+++ leetcode/051.NQueens/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include "Solution.h"
+using namespace std;
+int main()
+{
+    Solution su;
+    return 0;
+}
