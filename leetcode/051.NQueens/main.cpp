9e2abb6564a2f12048e292ffbf9bf518c3f5cfb3
niumeng07 <myliuda@sina.com>
2017-06-13 23:32:14 +0800

    051.NQueens 22ms

--- leetcode/051.NQueens/main.cpp
+++ leetcode/051.NQueens/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <vector>
-#include "Solution.h"
+#include "Solution2.h"
+#include <time.h>
 using namespace std;
 int main()
 {
     Solution su;
+	clock_t start, end;
+	start = clock();
+    vector<vector<string> > res = su.solveNQueens(8);
+	end = clock();
+    for(auto vec : res)
+    {
+        for (auto x : vec)
+            cout << x << ", ";
+        cout << endl;
+    }
+	cout << "Time used: " << end - start << endl;
     return 0;
 }
