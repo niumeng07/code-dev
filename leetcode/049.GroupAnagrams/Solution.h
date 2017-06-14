a6778fcb4edf970c6ec950a0b1aa5c9a9cb4c5ee
niumeng07 <myliuda@sina.com>
2017-06-14 08:19:45 +0800

    init 049.GroupAnagrams

---
+++ leetcode/049.GroupAnagrams/Solution.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string> > res;        
+    }
+};
---
+++ leetcode/049.GroupAnagrams/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Solution.h"
+
+using namespace std;
+
+int main()
+{
+    Sulution.su;
+    vector<string> strs({"eat", "tea", "tan", "ate", "nat", "bat"});
+    vector<vector<string> > res = su.groupAnagrams(strs);
+    return 0;
+}
