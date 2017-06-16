d5da9322207dfe94991c15e4222281d6cc28c554
niumeng07 <myliuda@sina.com>
2017-06-16 23:40:31 +0800

    047.PermutationII

--- leetcode/047.PermutationII/main.cpp
+++ leetcode/047.PermutationII/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include "Solution.h"
 
 using namespace std;
@@ -7,13 +8,17 @@ using namespace std;
 int main()
 {
     Solution su;
-    vector<int> nums;
+    clock_t start, end;
+    vector<int> nums({3,3,0,0,2,3,2});
+    start = clock();
     vector<vector<int> > res = su.permuteUnique(nums);
+    end = clock();
     for(auto vec : res)
     { 
         for( auto x : vec )
             cout << x << " ";
         cout << endl;
     }
+    cout << "permuteUnique time: " << end - start << endl;
     return 0;
 }
