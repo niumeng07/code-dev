a10d3f0a2540163fbf49595af0adb80eb5402731
niumeng07 <myliuda@sina.com>
2017-07-02 15:37:06 +0800

    088.MergeSortedArray. Rubbish, why merge [0] and [1] result [1]

---
+++ leetcode/088.MergeSortedArray/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "Solution.h"
+
+using namespace std;
+
+int main()
+{
+    Solution su;
+    vector<int> v1({1,2,5,8,8,10,11,12});
+    vector<int> v2({3,4,4,8,9,15});
+    su.merge(v1, v1.size(), v2, v2.size());
+    for(auto x : v1)
+        cout << x << " ";
+    cout << endl;
+    return 0;
+}
