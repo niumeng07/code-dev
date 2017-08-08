1bd2740e2e6d3e6da8ff93415c01505c52d85493
liuda <myliuda1990@gmail.com>
2017-08-08 08:57:06 +0800

    003.LongestSubStr 复习

--- leetcode/003.LongestSubstr/main.cpp
+++ leetcode/003.LongestSubstr/main.cpp
@@ -1,10 +1,14 @@
 #include "Solution.h"
+#include "Solution_B.h"
 #include <iostream>
 
 int main()
 {
     Solution su;
+    Solution_B su2;
     std::string str = "abcfdsfadqpei jakdjfadhjagh ajdfakldsjflqejr38rujaklejfaldhgadjflakjdfdlkajfkadjfkaljfabcbb";
+    str = "abadb";
     std::cout << su.lengthOfLongestSubstring(str) << std::endl;;
+    std::cout << su2.lengthOfLongestSubstring(str) << std::endl;;
     return 0;
 }
