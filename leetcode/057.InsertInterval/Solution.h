8215a96a9716cd5449702ec50837dc296f1d97b8
liuda <myliuda1990@gmail.com>
2017-11-28 23:23:25 +0800

    057.InsertInterval end

---
+++ leetcode/057.InsertInterval/Solution.h
@@ -0,0 +1,99 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/**
+ * Definition for an interval.
+ */
+class Interval 
+{
+public:
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+    void print_self()
+    {
+        cout << start << "\t" << end << endl;
+    }
+};
+void combine(vector<Interval>& intervals)
+{
+    for(vector<Interval>::iterator iter = intervals.begin(); iter != intervals.end() - 1; /**/)
+    {
+        vector<Interval>::iterator next = iter + 1;
+        if((*iter).end >= (*next).start)
+        {
+            if((*iter).end < (*next).end)
+            {
+                (*iter).end = (*next).end;
+                intervals.erase(next);
+            }
+            else 
+                intervals.erase(next);
+        }
+        else
+        {
+            iter++;
+        }
+    }
+}
+bool compare_intervals(Interval a, Interval b)
+{
+    if(a.start <= b.start)
+        return true;
+    else
+        return false;
+}
+class Solution 
+{
+public:
+    /*
+     * 1. 对每个个intervals的元素item，判断newInterval与item的关系
+     *    a. 若每一个item与newInterval无交集, 则把newInterval插入到intervals
+     *    b. 若item完全覆盖newInterval, 则直接返回intervals
+     *    c. 若item完全属于newInterval, 则用newInterval替代intervals中的item, 并运行2合并
+     *    d. 若item与newInterval有条件的相交,则..
+     * 2. 对每个item,若当前item的end大于下一个item的start，则合并
+     *    erase当前item,下个item的start设为当前item的start
+     * */
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        //for( Interval item : intervals)
+        for( int index = 0; index < intervals.size(); index++)
+        {
+            // b
+            if(intervals[index].start <= newInterval.start && intervals[index].end >= newInterval.end )
+                return intervals;
+            // c
+            else if(intervals[index].start > newInterval.start && intervals[index].end < newInterval.end )
+            {
+                intervals[index].start = newInterval.start;
+                intervals[index].end = newInterval.end;
+                combine(intervals);
+                return intervals;
+            }
+            //d
+            else if( intervals[index].start < newInterval.start && intervals[index].end < newInterval.end && intervals[index].end > newInterval.start)
+            {
+                intervals[index].end = newInterval.end;
+                combine(intervals);
+                return intervals;
+            }
+            else if( intervals[index].start > newInterval.start && intervals[index].end > newInterval.end && newInterval.end > intervals[index].start)
+            {
+                intervals[index].start = newInterval.start;
+                combine(intervals);
+                return intervals;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        // a
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(), compare_intervals);
+        combine(intervals);
+        return intervals;
+    }
+};
---
+++ leetcode/057.InsertInterval/main.cpp
@@ -0,0 +1,49 @@
+#include "Solution.h"
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+int main()
+{
+    Solution su;
+    vector<Interval> intervals;
+    /*
+    Interval i1(1,3);
+    intervals.push_back(i1);
+    Interval i2(6,9);
+    intervals.push_back(i2);
+    Interval ix(2,5);
+    */
+    Interval i1(1,2);
+    intervals.push_back(i1);
+    Interval i2(3,5);
+    intervals.push_back(i2);
+    Interval i3(6,7);
+    intervals.push_back(i3);
+    Interval i4(8,10);
+    intervals.push_back(i4);
+    Interval i5(12,16);
+    intervals.push_back(i5);
+    Interval ix(4,9);
+    /*
+    Interval i1(1,5);
+    intervals.push_back(i1);
+    Interval ix(5,9);
+    */
+    /*
+    Interval i1(1,5);
+    intervals.push_back(i1);
+    Interval ix(0,1);
+    */
+    cout << intervals.size() << endl;
+    for(auto inter : intervals)
+        inter.print_self();
+    clock_t start, end;
+    start = clock();
+    vector<Interval> result = su.insert(intervals, ix);
+    end = clock();
+    cout << "time: " << (end - start)/1000.0 << " ms" << endl;
+    for(auto inter : result)
+        inter.print_self();
+    return 0;
+}
