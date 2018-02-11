7f4982353fc7e2e6ab6e6c3531053ed3bf936b2f
liuda <myliuda1990@gmail.com>
2018-02-11 15:53:19 +0800

    val

--- leetcode/148.SortList/Solution.cpp
+++ leetcode/148.SortList/Solution.cpp
@@ -10,41 +10,38 @@ struct ListNode {
 int repartition(vector<ListNode*>& input, int left, int right){
     int middle = right;
     int i = left, j = right - 1;
-    while(true){
+    while(true) {
         if(i >= j )
             break;
         if(input[i]->val > input[middle]->val && input[j]->val < input[middle]->val){
             swap(input[i], input[j]);
             i++;
             j--;
-        } else if(input[i]->val < input[middle]->val){
+        } else if(input[i]->val <= input[middle]->val) {
             i++;
-        } else if(input[j]->val > input[middle]->val){
+        } else if(input[j]->val >= input[middle]->val) {
             j--;
         }
     }
-    if(input[j] > input[middle])
-        swap(input[j], input[middle]);
-    return j;
+    if(input[j]->val > input[right]->val)
+        swap(input[j], input[right]);
+    return i;
 }
-void quick_sort(vector<ListNode*>& input, int left, int right){
+void quick_sort(vector<ListNode*>& input, int left, int right) {
     if(right <= left)
         return;
-    if(right == left + 1){
+    if(right > left + 1) {// 3 1 0
+        int middle = repartition(input, left, right);//middle always 0
+        quick_sort(input, left, middle);
+        quick_sort(input, middle, right);//这里就是死循环
+    } else if(right == left + 1) {
         if(input[left]->val > input[right]->val)
             swap(input[left], input[right]);
-        return;
-    }
-    if(right > left + 1){
-        int middle = repartition(input, left, right);
-        quick_sort(input, left, middle);
-        quick_sort(input, middle, right);
-        return;
     }
 }
-void print_listnode(ListNode* head){
+void print_listnode(ListNode* head) {
     ListNode* iter = head;
-    while(iter != NULL){
+    while(iter != NULL) {
         cout << iter->val << "\t";
         iter = iter->next;
     }
@@ -53,17 +50,18 @@ void print_listnode(ListNode* head){
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
+        if(head==NULL)
+            return NULL;
         vector<ListNode*> listnode_vec;
         ListNode* iter = head;
         while(iter!=NULL){
             listnode_vec.push_back(iter);
             iter = iter->next;
         }
-        //采用quick_sort方式
         quick_sort(listnode_vec, 0, listnode_vec.size() - 1 );
         head = listnode_vec[0];
         iter = head;
-        for(int i = 1; i < listnode_vec.size(); i++){
+        for(int i = 1; i < listnode_vec.size(); i++) {
             iter->next = listnode_vec[i];
             iter = iter->next;
         }
@@ -73,11 +71,11 @@ public:
 };
 
 int main(){
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(3);
-    head->next->next = new ListNode(2);
-    print_listnode(head);
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(1);
+    head->next->next = new ListNode(0);
     Solution su;
-    su.sortList(head);
     print_listnode(head);
+    ListNode* t = su.sortList(head);
+    print_listnode(t);
 }
