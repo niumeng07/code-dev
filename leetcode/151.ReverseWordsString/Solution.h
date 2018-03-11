6203e3a5cfd0d2581bfabb834b9653e63a5e9491
yangxing <18301292502@163.com>
2018-03-11 22:19:43 +0800

    151 O(1) Space test

--- leetcode/151.ReverseWordsString/Solution.h
+++ leetcode/151.ReverseWordsString/Solution.h
@@ -5,6 +5,7 @@ using namespace std;
 
 class Solution {
 public:
+    //O(N)空间复杂度的，不符合要求
     void reverseWords(string &s) {
         vector<string> stack;
         int left = 0;
@@ -19,6 +20,31 @@ public:
             s = stack[stack.size() - 1];
         for (int j = stack.size() - 2; j >= 0; j--) {
             s = s + " " + stack[j];
+        }
+    }
+    
+    void reverseWords2(string &s) {
+        int ll = 0, lr = 0;
+        int rl = s.size() - 1, rr = s.size() - 1;
+        while (true) {
+            if ( rl < lr )
+                break;
+            if (s[lr] != ' ') {
+                lr++;
+                continue;
+            }
+            if (s[rl] != ' ') {
+                rl--;
+                continue;
+            }
+            //the sky is blue
+            //   .      .
+            s = s.substr(rl + 1, rr - rl) + " " + s.substr(lr + 1, rl - 1 - lr) + " " + s.substr(ll, lr - ll + 1);
+            cout << s << endl;
+            ll = lr + 1 + (rr - rl - (lr - ll + 1));
+            rr = rl - 1 - ( rl - rr + (lr - ll + 1));
+            lr = ll + 1;
+            rl = rr - 1;
         }
     }
 };
--- leetcode/151.ReverseWordsString/main.cpp
+++ leetcode/151.ReverseWordsString/main.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main() {
     Solution su;
     string input = "the sky is blue";
-    su.reverseWords(input);
+    su.reverseWords2(input);
     cout << input << endl;
 }
