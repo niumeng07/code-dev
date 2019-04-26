e48216cab7394669fb6bd0651eccf116274b46e9
liuda <myliuda1990@gmail.com>
2019-04-26 10:04:22 +0800

    029.DivideTwoInteger

--- leetcode/029.DivideTwoInteger/Solution.h
+++ leetcode/029.DivideTwoInteger/Solution.h
@@ -1,12 +1,35 @@
 #include <iostream>
 #include <limits.h>
+#include <math.h>
 using namespace std;
 
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        int result = 0;
-        int search = 1;
-        return result;
+  int divide(int a, int b) {
+    int flag = 1;
+    if ((a < 0 && b > 0) || (a > 0 && b < 0))
+      flag = -1;
+    a = abs(a);
+    b = abs(b);
+    unsigned int x, y;
+    int ans = 0;
+    while (a >= b) {
+      x = b;
+      y = 1;
+      cout << x << "\t" << y << endl;
+      while (a >= (x << 1)) {
+        x <<= 1;
+        y <<= 1;
+        cout << x << "\t" << y << endl;
+      }
+      a -= x;
+      ans += y;
     }
+    ans = 0 - ans;
+    return ans;
+  }
 };
+void test() {
+  cout << "3*7=21  =  ((3<<2)+(3<<1)+3)"
+       << "=" << ((3 << 2) + (3 << 1) + 3) << endl;
+}
--- leetcode/029.DivideTwoInteger/main.cpp
+++ leetcode/029.DivideTwoInteger/main.cpp
@@ -1,14 +1,10 @@
-#include <iostream>
 #include "Solution.h"
+#include <iostream>
 using namespace std;
-int main()
-{
-    Solution su;
-    cout << su.divide( 4,2) << endl;
-    cout << su.divide( 14,3) << endl;
-    cout << su.divide( -14,3) << endl;
-    cout << su.divide( -14,-3) << endl;
-    cout << su.divide( 1,0) << endl;
-    cout << su.divide(2147483647,1) << endl;
-    cout << su.divide(-2147483648,-1) << endl;
+
+int main() {
+  Solution su;
+  cout << 4 / 2 << endl << su.divide(4, 2) << endl;
+
+  //test();
 }
