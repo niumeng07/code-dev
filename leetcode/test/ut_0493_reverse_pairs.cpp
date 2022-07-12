29c8bd5a70f060ab05b115d3e048e8dac17b4c16
niumeng07 <myliuda@sina.com>
2022-07-12 13:13:07 +0800

    include/gtest   test/ut_0263_ugly_number.cpp   test/ut_0493_reverse_pairs.cpp   test/ut_0863_all_nodes_distance_k_in_binary_tree.cpp

--- leetcode/test/ut_0493_reverse_pairs.cpp
+++ leetcode/test/ut_0493_reverse_pairs.cpp
@@ -1,10 +1,11 @@
 #include <string>
 #include <vector>
 #include "gtest/gtest.h"
-#include "0263_ugly_number/ugly_number.h"
+#include "0493_reverse_pairs/reverse_pairs.h"
 
-TEST(ugly_number0, test0) {
-    EXPECT_EQ(Solution().isUgly(2), true);
+TEST(reverse_pairs0, test0) {
+    vector<int> nums({1, 3, 2, 3, 1});
+    EXPECT_EQ(Solution().reversePairs(nums), 2);
 }
 
 int main(int argc, char **argv) {
